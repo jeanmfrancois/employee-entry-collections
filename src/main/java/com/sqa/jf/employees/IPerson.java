3ed8e850671a13421311f7de2c9edfcb3abcce2c
Jean-francois Nepton <jeanmfrancois@hotmail.com>
2015-12-19 16:00:37 -0800

    Adding IPerson Interface

---
+++ src/main/java/com/sqa/jf/employees/IPerson.java
@@ -0,0 +1,28 @@
+/**
+ * File Name: IPerson.java<br>
+ * Nepton, Jean-francois<br>
+ * Java Boot Camp Exercise<br>
+ * Instructor<br>
+ * Created: Dec 19, 2015
+ */
+package com.sqa.jf.employees;
+
+/**
+ * IPerson //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ *
+ * @author Nepton, Jean-francois
+ * @version 1.0.0
+ * @since 1.0
+ */
+public interface IPerson {
+
+	final static int MAX_AGE = 110;
+
+	public String getName();
+
+	public boolean move();
+}
